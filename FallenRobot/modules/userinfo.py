2e3772b3e800c4fe23e1b4b6e451218e343ff9f2
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-07-18 18:22:07 +0530

    Update userinfo.py
    Signed-off-by: Dkmovie <99384833+Dkmovie@users.noreply.github.com>

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -301,10 +301,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/FallenAssociation/7"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/movie_a1"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/FallenAssociation/8"
+                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/movie_on1"
                             ),
                         ],
                     ]
